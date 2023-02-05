77260ae481fe550abd2d06d09fcd07a77853d2e0
Robert Kozin <robert@robertkozin.com>
2023-02-05 18:53:35 -0500

    update rpc_server.py

--- minecraft/pi/rpc_server.py
+++ minecraft/pi/rpc_server.py
@@ -5,18 +5,9 @@ import socketserver
 from struct import pack, unpack
 import threading
 
-<<<<<<< Updated upstream
-class RPCHandler(socketserver.BaseRequestHandler):
-    slam = None
-
-    def __init__(self, slam):
-        self.slam = slam
-
-=======
 slam = None
 
 class RPCHandler(socketserver.BaseRequestHandler):
->>>>>>> Stashed changes
     def handle(self):
         while True:
             fn, = unpack('!b', self.request.recv(1))
@@ -27,21 +18,6 @@ class RPCHandler(socketserver.BaseRequestHandler):
                 self.get_nodes()
             elif fn == 3:
                 pass # upsert_nodes, unimplemented because no graphics 
-<<<<<<< Updated upstream
-            elif fn == 4:
-                self.start_recording()
-            elif fn == 5:
-                self.stop_recording()
-            elif fn == 6:
-                self.reset_recording()
-    
-    def get_location(self):
-        x, y, z, yaw = self.slam.get_location_from_gps()
-        self.request.sendall(pack('!ffff', x, y, z, yaw))
-    
-    def get_nodes(self):
-        nodes = self.slam.get_nodes()
-=======
             elif fn == 10:
                 self.start_recording()
             elif fn == 11:
@@ -55,7 +31,6 @@ class RPCHandler(socketserver.BaseRequestHandler):
     
     def get_nodes(self):
         nodes = slam.get_nodes()
->>>>>>> Stashed changes
 
         self.request.send(pack('!i', len(nodes)))
 
@@ -64,15 +39,6 @@ class RPCHandler(socketserver.BaseRequestHandler):
             self.request.sendall(pack('!iiifff', nodeId, playerId, seqId, x, y, z))
 
     def start_recording(self):
-<<<<<<< Updated upstream
-        self.slam.start_recording()
-
-    def stop_recording(self):
-        self.slam.start_recording()
-
-    def reset_recording(self):
-        self.slam.reset_recording()
-=======
         slam.start_recording()
 
     def stop_recording(self):
@@ -80,20 +46,14 @@ class RPCHandler(socketserver.BaseRequestHandler):
 
     def reset_recording(self):
         slam.reset_recording()
->>>>>>> Stashed changes
 
 class ThreadedRPCHandler(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-<<<<<<< Updated upstream
-def start_rpc_server(slam, host, port):
-    server = ThreadedRPCHandler((host, port), RPCHandler(slam))
-=======
 def start_rpc_server(_slam, host, port):
     global slam
     slam = _slam
     server = ThreadedRPCHandler((host, port), RPCHandler)
->>>>>>> Stashed changes
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
     server_thread.start()
